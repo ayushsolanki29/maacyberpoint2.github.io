4a580ad50a1f9f900fa08dc550f0615fb7e9bd86
Ayush Solanki <103575160+ayushsolanki29@users.noreply.github.com>
2023-02-08 18:45:56 +0530

    Add files via upload

--- source/file2.js
+++ source/file2.js
@@ -1,3 +1,10 @@
+
+document.querySelector("body").insertAdjacentHTML("beforeend", '<div class="loader"><span class="bar"></span><span class="bar"></span><span class="bar"></span></div>');
+
+window.addEventListener("load", function () {
+  document.querySelector(".loader").remove();
+});
+
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
@@ -10,7 +17,7 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
+	if (scroll_position > 50) {
 		header.style.backgroundColor = '#29323c';
 	} else {
 		header.style.backgroundColor = 'transparent';
@@ -22,4 +29,20 @@ menu_item.forEach((item) => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
+});
+
+const shareButton = document.getElementById("shareBtn");
+const title = window.document.title;
+const url = window.document.location.href;
+shareButton.addEventListener("click", () => {
+  if (navigator.share) {
+    navigator
+      .share({ title: `${title}`, url: `${url}` })
+      .then(() => {
+        console.log("thanks For sharing!");
+      })
+      .catch(console.error);
+  } else {
+   document.write("Please use Mobile Device");
+  }
 });
